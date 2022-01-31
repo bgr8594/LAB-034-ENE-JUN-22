f8e2f399f1804c3ba72a351d020a5bc39cd85d34
rtrevinnoc <rtrevinnoc@wearebuildingthefuture.com>
2022-01-31 01:20:42 -0600

    Pagina presupuesto agregada

--- ionic-basic/src/app/presupuesto/presupuesto.page.ts
+++ ionic-basic/src/app/presupuesto/presupuesto.page.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-presupuesto',
-  templateUrl: './presupuesto.page.html',
-  styleUrls: ['./presupuesto.page.scss'],
+	selector: 'app-presupuesto',
+	templateUrl: './presupuesto.page.html',
+	styleUrls: ['./presupuesto.page.scss'],
 })
 export class PresupuestoPage implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+	public gastos: string[] = ['Directos', 'Fijos', 'Variables'];
+	public selected: any;
+	public amount: number;
+	public results: string;
+	public errors: string='light';
+
+	constructor() {}
+
+	ngOnInit() {}
+
+	customPopoverOptions: any = {
+		header: 'Gastos',
+		subHeader: 'Seleccion de tipo de gastos',
+		message: 'Seleccione un unico tipo de gasto.'
+	};
+
+	cambioValor(value) {
+		console.log(value);
+	}
+
+	guardar() {
+		if(this.amount != null && this.selected != null) {
+			this.errors = 'success';
+			this.results = 'Tipo de gasto seleccionado: ' + this.selected + ' \nMonto: ' + this.amount;
+		} else {
+			this.errors = 'danger';
+			this.results = "¡Debe completar el formulario!";
+		}
+	}  
 
 }
