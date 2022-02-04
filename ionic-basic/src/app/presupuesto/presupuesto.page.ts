12c13b4701df8396a3abdb41065c06a50ce3b816
GuillermoBlancarte <guillermooblancarte@gmail.com>
2022-02-03 23:32:06 -0600

    Practica1

--- ionic-basic/src/app/presupuesto/presupuesto.page.ts
+++ ionic-basic/src/app/presupuesto/presupuesto.page.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PresupuestoPage implements OnInit {
 
+  public gastos: string[]=['Directos','Fijos', 'Variables'];
+  public selectedValue: any;
+  public monto: number;
+  public resultados: string;
+  public errResultados: string='light';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  customPopoverOptions: any = {
+    header: 'Seleccion de gasots',
+    subHeader: 'Seleccione el tipo de gasto',
+    message: 'Solo seleccione un tipo de gasto'
+  };
+
+  cambioValor(value){
+    console.log(value);
+  }
+  guardar(){
+    this.resultados ="";
+    if(this.monto!=null && this.selectedValue!=null){
+      this.errResultados = 'success';
+      this.resultados = 'Gasto seleccionado: '+this.selectedValue+' \nMonto: '+this.monto+'\n';
+    }
+    else{
+      this.errResultados = 'danger';
+      this.resultados ="No a completado los campos del formulario";
+    }
+  }  
+  
 }
