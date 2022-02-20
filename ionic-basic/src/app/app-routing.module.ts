734d021d1216576d53072d0bfb9a09e4ae86c570
Azt3kCode <alexis.aets18@gmail.com>
2022-02-19 20:54:54 -0600

    Practica 4

--- ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic/src/app/app-routing.module.ts
@@ -16,17 +16,17 @@ const routes: Routes = [
     loadChildren: () => import('./presupuesto/presupuesto.module').then( m => m.PresupuestoPageModule)
   },
   {
+    path: 'alumnos',
+    loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule)
+  },  {
     path: 'inicio',
     loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
   },
   {
     path: 'reciever',
     loadChildren: () => import('./reciever/reciever.module').then( m => m.RecieverPageModule)
-  },
-  {
-    path: 'alumnos',
-    loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule)
   }
+
 ];
 
 @NgModule({
--- ionic-basic/src/app/reciever/reciever.page.ts
+++ ionic-basic/src/app/reciever/reciever.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Personaje } from '../models/personaje.model';
+import { ServicioCService } from '../services/servicio-c.service';
 
 @Component({
   selector: 'app-reciever',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reciever.page.scss'],
 })
 export class RecieverPage implements OnInit {
+  user: any;
+  list: any[];
+  personajes: Personaje[];
 
-  constructor() { }
+  constructor(private servicioCService: ServicioCService) { }
 
   ngOnInit() {
+    this.servicioCService.$getObjectSource.subscribe(data=>{
+      console.log(data);
+      this.user = data;
+    }).unsubscribe();
+    this.servicioCService.$getListSource.subscribe(data=>{
+      console.log(data);
+      this.list = data;
+    }).unsubscribe();
+
+    this.servicioCService.getPersonajes().subscribe((response: any)=>{
+      this.personajes = response.results;
+    });
   }
 
 }
