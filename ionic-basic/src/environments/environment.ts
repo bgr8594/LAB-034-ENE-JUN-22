3fa7be4237244ca34a6af4b44e0060b41fdd6266
RobertoPnt <robe.3536@gmail.com>
2022-03-28 16:05:26 -0600

    Practicia 7

--- ionic-basic/src/environments/environment.ts
+++ ionic-basic/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCSKJRLqJoJMNY2oghCg2RFaUDh5ZDMw1Q",
+    authDomain: "back-front-363c6.firebaseapp.com",
+    projectId: "back-front-363c6",
+    storageBucket: "back-front-363c6.appspot.com",
+    messagingSenderId: "1079896107554",
+    appId: "1:1079896107554:web:fee6351b91959b4856387b",
+    measurementId: "G-5MZDP63VWP"
+  } 
 };
 
 /*
