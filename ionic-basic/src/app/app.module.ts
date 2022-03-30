531d5f3a302a10a46bc4f7cdd29370a192907a44
rtrevinnoc <rtrevinnoc@wearebuildingthefuture.com>
2022-03-30 08:18:37 -0600

    Practica 7: Firebase login

--- ionic-basic/src/app/app.module.ts
+++ ionic-basic/src/app/app.module.ts
@@ -3,17 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { HttpClientModule } from '@angular/common/http';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy, NavParams } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ComponentesModule } from './componentes/componentes.module';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import { environment } from 'src/environments/environment';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBcsNGNxLwWnJd4up26JmXbRgZ0skC5bXc",
+  authDomain: "practica7-4a3e3.firebaseapp.com",
+  projectId: "practica7-4a3e3",
+  storageBucket: "practica7-4a3e3.appspot.com",
+  messagingSenderId: "541474999039",
+  appId: "1:541474999039:web:8626e48901e582292d1aca",
+  measurementId: "G-CSSPYG9X1E"
+}
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [ComponentesModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule ],
-  providers: [HttpClientModule,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [ComponentesModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule, AngularFireModule.initializeApp(firebaseConfig), AngularFireAuthModule],
+  providers: [NavParams, HttpClientModule,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
