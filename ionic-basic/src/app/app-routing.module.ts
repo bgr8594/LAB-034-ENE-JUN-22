a8fc732e095e08b195099a1b3c23b8c682112137
RamGarza <julian.ramirezgrz@uanl.edu.mx>
2022-04-02 14:54:10 -0600

    Practica 9

--- ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic/src/app/app-routing.module.ts
@@ -50,7 +50,11 @@ const routes: Routes = [
   {
     path: 'admin',
     loadChildren: () => import('./admin/admin.module').then( m => m.AdminPageModule)
+  },  {
+    path: 'destinos',
+    loadChildren: () => import('./destinos/destinos.module').then( m => m.DestinosPageModule)
   }
+
 
 
 ];
