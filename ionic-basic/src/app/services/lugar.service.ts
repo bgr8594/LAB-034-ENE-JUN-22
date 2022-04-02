f5dd3c240388a3ea03e510f6fa894debe0e5b28b
JM1897962 <1897962.jmmr@gmail.com>
2022-04-01 19:10:10 -0600

    practica 10 - 1897962

--- ionic-basic/src/app/services/lugar.service.ts
+++ ionic-basic/src/app/services/lugar.service.ts
@@ -14,7 +14,7 @@ export class LugarService {
       nombre:lugar.nombre,
       ubicacion: {longitud:'', latitud:''}
     };
-    this.dbFirestore.collection('lugar').add(lugarTemp);
+    return this.dbFirestore.collection('lugar').add(lugarTemp);
   }
 
   async getLugares(destinos: Lugar[]){
@@ -35,6 +35,19 @@ export class LugarService {
     });
 
   }
+
+  getLugaresChanges(){
+    return this.dbFirestore.collection('lugar').snapshotChanges();
+  }
+
+  updateLugares(id: any, lugar: any){
+   return this.dbFirestore.collection('lugar').doc(id).update(lugar);
+  }
+
+  deleteLugar(id: any){
+    return this.dbFirestore.collection('lugar').doc(id).delete();
+  }
+
 }
 
 //lugar.service.spec.ts
