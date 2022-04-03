d1433be7d0993d8179fd09b83e1ad84f941664c0
JaimePach <jg823726@gmail.com>
2022-04-03 18:59:06 -0500

    Practica 4 1.0

---
+++ ionic-basic/src/app/services/servicio-c.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicioCService {
+
+  private objectSource = new BehaviorSubject<{}>({});
+  private listSource = new BehaviorSubject<any[]>([]);
+
+  $getListSource :any  //this.objectSource.asObservable();
+  $getObjectSource :any //this.listSource.asObservable();
+  
+  constructor(private http:HttpClient) { }
+  
+  sendObjectSource(data: any){
+    this.objectSource.next(data);
+  }
+
+  sendListSource(data: any[]){
+    this.listSource.next(data);
+  }
+
+  getPersonajes() :Observable<any>{
+    return this.http.get<any>(`http://swapi.dev/api/people/`,{});
+  }
+
+  getPersonaje(idPersonaje:string): Observable<any>{
+    return this.http.get<any>(`http://swapi.dev/api/people/${idPersonaje}`,{});
+  }
+}
