e85a300d6b131963e72f8b2a02743173a668c47a
bgr8594 <bgr8594@gmail.com>
2022-04-04 20:38:32 -0500

    practica 11, consumir api por metodos HTTP GET, PUT, POST, DELETE, front ionic-->Java spring-boot-->firebase

--- ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic/src/app/app-routing.module.ts
@@ -55,7 +55,12 @@ const routes: Routes = [
   {
     path: 'destinos', canActivate:[AuthGuard],
     loadChildren: () => import('./destinos/destinos.module').then( m => m.DestinosPageModule)
+  },
+  {
+    path: 'destinos-api', canActivate:[AuthGuard],
+    loadChildren: () => import('./destinos-api/destinos-api.module').then( m => m.DestinosApiPageModule)
   }
+
 
 ];
 
