2865e1ed0a19e4221041ea52b6afdff3b6be20f9
DeyLuna <david.alejandro.luna.martinez1@gmail.com>
2022-04-09 22:55:29 -0500

    Practica 09

--- ionic-basic/src/app/componentes/float-menu/float-menu.component.ts
+++ ionic-basic/src/app/componentes/float-menu/float-menu.component.ts
@@ -52,6 +52,8 @@ export class FloatMenuComponent implements OnInit {
       icono:'cash-outline'},
       {nombre: 'inicio',enlace:'/inicio',
       icono:'navigate-outline'},
+      {nombre: 'Turismo',enlace:'/destinos',
+      icono:'airplane'},
       {nombre: 'Tabs',enlace:'/tabs',
       icono:'folder-outline'},
       {nombre: 'login',enlace:'/login',
