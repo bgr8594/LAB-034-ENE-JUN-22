dc575898e0032fba1a236614a7eb3c6c1f4b58b8
PepeRocha19 <89224430+PepeRocha19@users.noreply.github.com>
2022-04-11 16:24:38 -0500

    Practica 11

--- src/app/models/lugar.model.ts
+++ src/app/models/lugar.model.ts
@@ -1,14 +1,9 @@
 export class Lugar {
     nombre: string;
     id?: string;
-    ubicacion?:{latitud:string, longitud:string}
-    
-    constructor(){
-        this.nombre=''
-    }
-
-    public setUbicacion(latitud: string, Longiutd: string){
-        this.ubicacion.latitud= latitud;
-        this.ubicacion.longitud = Longiutd;
-    }
+    latitud:number; 
+    longitud:number;
+        constructor(){
+            this.nombre=''
+        }
 }
