ede011fa858e51d5a829ff38c88827aef6ec3948
Seb4st <sebas2003@live.com.mx>
2022-05-01 18:21:02 -0500

    Practica 4

--- ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic/src/app/app-routing.module.ts
@@ -6,23 +6,31 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },  {
-    path: 'presupuesto',
-    loadChildren: () => import('./presupuesto/presupuesto.module').then( m => m.PresupuestoPageModule)
   },
+
   {
     path: 'presupuesto',
     loadChildren: () => import('./presupuesto/presupuesto.module').then( m => m.PresupuestoPageModule)
   },
+
   {
     path: 'alumnos',
     loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule)
+  },  
+  
+  {
+    path: 'inicio',
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule),
   },
-
+  {
+  path: 'reciever',
+  loadChildren: () => import('./reciever/reciever.module').then( m => m.RecieverPageModule),
+  }
 ];
 
 @NgModule({
@@ -31,4 +39,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { } 
