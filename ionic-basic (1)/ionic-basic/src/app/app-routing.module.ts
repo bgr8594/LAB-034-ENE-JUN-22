9664ab19075cbbbab1f2cf911355400e25976cb2
BloodShotMK <rdelaros2404@gmail.com>
2022-05-22 11:46:55 -0500

    Practica 09

--- ionic-basic (1)/ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic (1)/ionic-basic/src/app/app-routing.module.ts
@@ -51,6 +51,14 @@ const routes: Routes = [
   {
     path: 'admin', canActivate:[AuthGuard],
     loadChildren: () => import('./admin/admin.module').then( m => m.AdminPageModule)
+  },
+  {
+    path: 'destinos', canActivate:[AuthGuard],
+    loadChildren: () => import('./destinos/destinos.module').then( m => m.DestinosPageModule)
+  },
+  {
+    path: 'destinos',
+    loadChildren: () => import('./destinos/destinos.module').then( m => m.DestinosPageModule)
   }
 
 ];
