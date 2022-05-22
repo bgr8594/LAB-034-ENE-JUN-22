20c9b90e7798762fcf9438aa56793c04b6e7363e
DeyLuna <david.alejandro.luna.martinez1@gmail.com>
2022-05-22 18:57:16 -0500

    Practica 12, GoogleMaps
    Usar la api de Google Maps para implementar un maoa y poder colocar un marcador a un destino

--- ionic-basic/src/environments/environment.ts
+++ ionic-basic/src/environments/environment.ts
@@ -5,14 +5,15 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBQ4z_C7VhwsGLq0I4io-G16HJzK0y-R6Y",
-    authDomain: "back-front-5826c.firebaseapp.com",
-    projectId: "back-front-5826c",
-    storageBucket: "back-front-5826c.appspot.com",
-    messagingSenderId: "661367446314",
-    appId: "1:661367446314:web:c887a8156b109289235c53",
-    measurementId: "G-5YR2H2NT8B"
-  }
+  apiKey: "AIzaSyBQ4z_C7VhwsGLq0I4io-G16HJzK0y-R6Y",
+  authDomain: "back-front-5826c.firebaseapp.com",
+  projectId: "back-front-5826c",
+  storageBucket: "back-front-5826c.appspot.com",
+  messagingSenderId: "661367446314",
+  appId: "1:661367446314:web:c887a8156b109289235c53",
+  measurementId: "G-5YR2H2NT8B"
+  },
+  apikeyMaps: 'AIzaSyCNwUX8OOYHbzPfpeNJcPLvMF8_xcV3M1M'
 };
 
 /*
